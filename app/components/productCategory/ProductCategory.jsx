7516d9510847232f99f2e7ddb3e9911660e4f0b3
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-06-11 23:19:40 +0600

    update

--- app/components/productCategory/ProductCategory.jsx
+++ app/components/productCategory/ProductCategory.jsx
@@ -19,7 +19,7 @@ const ProductCategory = () => {
             </div>
             <div className="sm:bg-white sm:text-black rounded-full w-1/4 mx-2 flex items-center sm:flex-row flex-col sm:py-2 py-0 sm:px-2 px-0">
               <RiHomeSmileFill className="sm:text-xl text-[3rem]" />
-              <div className="text-xs sm:text-sm sm:mx-3 mx-0">Furniture</div>
+              <div className="text-xs sm:text-sm sm:mx-3 mx-0">Furnituree</div>
             </div>
           </div>
         </div>
